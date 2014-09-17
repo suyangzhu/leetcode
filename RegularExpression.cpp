8ba95c5928563803127ef286cb155e29c3178bab
suyangzhu <suyangzhu@gmail.com>
2014-09-17 16:42:54 -0500

    add recursion solution to regular expression matching

--- RegularExpression.cpp
+++ RegularExpression.cpp
@@ -8,85 +8,19 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(const char *s, const char *p) {
-	    stack<char> strStack;
-	    stack<char> regex;
-	    int i = 0;
-	    while (s[i] != '\0') 
-		   strStack.push(s[i++]);
-	    i = 0;
-	    while (p[i] != '\0')
-		    regex.push(p[i++]);
-	    char last;
-	    while (!strStack.empty()) {
-		    char tmp = strStack.top();
-		    strStack.pop();
-		    if (regex.empty()) 
-			    return false;
-		    if (tmp == regex.top() || regex.top() == '.') {
-			    regex.pop();
-		    } else
-	            if (regex.top() == '*') {
-			    if (regex.empty()) {
-				    return false;
-			    }
-			    regex.pop();
-			    
-			    if (regex.top() == '.' || regex.top() == tmp) {
-				    regex.push('*');
-				    if (strStack.empty())
-					    last = tmp;
-			    } else {
-				    if (regex.empty()) {
-					    return false;
-				    }
-				    regex.pop();
-				    if (regex.empty())
-				    {
-					    return false;
-				    }
-				    //keep removing the * which does not match
-				    bool quit = false;
-				    while (regex.top() == '*') {
-					    regex.pop();
-					    // *match, do not pop;
-					    if (regex.top() == '.' || regex.top() == tmp) {
-						    regex.push('*');
-						    quit = true;
-						    break;
-					    } else {
-						    regex.pop();
-					    }
-				    }
-				    if (quit)
-					    continue;
-				    if (regex.empty() || (regex.top() != tmp &&regex.top() != '.'))  {
-					    return false;
-				    }
-				    else 
-					    // match. pop from regex stack;
-					    regex.pop();
-				    
-			    }
-		    } else {
-			    return false;
-		    }
-	    }
-	    if (regex.empty()) 
-		    return true;
-	    else {
-		    while (!regex.empty() && regex.top() == '*') {
-			    regex.pop();
-			    regex.pop();
-		    }
-		    if (regex.empty())
-			    return true;
-		    else  {
-			    if (regex.top() == last || regex.top() =='.') 
-				    regex.pop();
-			    if (regex.empty())
-				    return true;
-			    return false;
-		    }
+	    if (*p == '\0') return *s == '\0';
+	    if (*(p+1) == '*') {		// 0 to a number of substr can be matched using the char
+		    				// before star.
+	    	while (*s && (*s == *p || *p == '.')) {
+			if (isMatch(s, p+2))	// there is no need to consume all the chars can be match.
+						// example: aabbaa aab*baa. If * consumes all the b. the string 
+						// cannot match. So we need to match each time.  
+				return true;
+			s++;
+		}
+		return isMatch(s, p+2);		// consume all the chars that * can match.
+	    } else {
+		    return *s && (*s == *p || *p == '.') && isMatch(s+1, p+1);
 	    }
     }
 };

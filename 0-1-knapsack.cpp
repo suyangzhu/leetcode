4c0d5c100adcce85a14929fbef81922a4a4e2282
suyangzhu <suyangzhu@gmail.com>
2014-11-26 14:01:44 -0600

    add knapsack

---
+++ 0-1-knapsack.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Knapsack(int target, vector<int> &value, vector<int> &weight, int n) {
+	if (target == 0 || n == 0) return 0;
+}
+
+
--- LongestIncreasingSubsequence.cpp
+++ LongestIncreasingSubsequence.cpp
@@ -13,9 +13,10 @@ int max(int first, int second) {
 vector<int> LongestIncreasingSubsequence(const int* A, int n) {
 	vector<int> prev(n, -1);
 	vector<int> lis(n, 0);
+	lis[0] = 1;
 	vector<int> result;
 	for (int i = 1; i < n; i++) {
-		int maxLen = 0;
+		int maxLen = 1;
 		for (int k = 0; k < i; k++) {
 			if (A[i] > A[k]) {
 				if (maxLen < lis[k]+1) {
@@ -39,12 +40,20 @@ vector<int> LongestIncreasingSubsequence(const int* A, int n) {
 		result.push_back(A[idx]);
 		idx = prev[idx];
 	}
+	for (int i = 0; i< n; i++) {
+		std::cout << lis[i] << " ";
+	}
+	std::cout << std::endl;
 	reverse(result.begin(), result.end());
 	return result;
 }
 
 int main() {
 	int A[11] = {66, 92, 123, 31, 83, 53, 48, 17, 9, 57, 75};
+	for (int i = 0; i < 11; i++) {
+		std::cout << A[i] << " ";
+	}
+	std::cout << std::endl;
 	vector<int> result = LongestIncreasingSubsequence(A, 11);
 	for (int i = 0; i < result.size(); i++) {
 		std::cout << result[i] << " ";
